a4fd6f5f8c6433f57177d0fef02c081ee6b93092
Jonathan Andrews <jonathanandrewsuk@gmail.com>
2017-02-24 16:00:23 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,60 @@
+def instantiate_new_array
+
+empty_array = []
+
+end
+
+def array_with_two_elements
+
+two_element_array = ["first", "second"]
+
+end
+
+
+def first_element(taylor_swift)
+
+#two_element_array = ["first", "second"]
+
+taylor_swift[0]
+
+end
+
+def third_element(taylor_swift)
+
+#three_element_array = ["first", "second", "third"]
+
+taylor_swift[2]
+
+end
+
+def last_element(taylor_swift)
+
+#three_element_array = ["first", "second", "third"]
+
+taylor_swift[-1]
+
+end
+
+def first_element_with_array_methods(taylor_swift)
+
+#three_element_array = ["first", "second", "third"]
+
+taylor_swift.first
+
+end
+
+def last_element_with_array_methods(taylor_swift)
+
+#three_element_array = ["first", "second", "third"]
+
+taylor_swift.last
+
+end
+
+def length_of_array(taylor_swift)
+
+#three_element_array = ["first", "second", "third"]
+
+taylor_swift.count
+
+end
