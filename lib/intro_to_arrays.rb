2b69d7a400ec6c53e7b2b96786f33febb7079431
brian-hopman <hopman_2@hotmail.com>
2017-03-24 23:35:54 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  ary = []
+end
+
+
+def array_with_two_elements
+  ary = [1,2]
+end
+
+def first_element(ary)
+  ary[0]
+end
+
+def third_element(ary)
+  ary[2]
+end
+
+def last_element(ary)
+  ary[-1]
+end
+
+def first_element_with_array_methods(ary)
+  ary.shift
+end
+
+def last_element_with_array_methods(ary)
+  ary.pop
+end
+
+def length_of_array(ary)
+  ary.length
+end
