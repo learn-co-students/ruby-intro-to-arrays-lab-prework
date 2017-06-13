8ce95ba6923bc2fa365f9806228fb950c6252443
gabidavila <gabidavila@gmail.com>
2017-06-13 17:46:24 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [0 , 1]
+end
+
+def first_element(collection)
+  collection.first
+end
+
+def third_element(collection)
+  collection[2]
+end
+
+def last_element(collection)
+  collection.last
+end
+
+def first_element_with_array_methods(collection)
+  collection.first
+end
+
+def last_element_with_array_methods(collection)
+  collection.last
+end
+
+def length_of_array(collection)
+  collection.length
+end
