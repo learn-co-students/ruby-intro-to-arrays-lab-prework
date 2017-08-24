79ed026a8b18d4a5b161d332fefdd81b27296ae3
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-24 09:52:13 -0400

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,33 +1,47 @@
 def instantiate_new_array
-    my_new_array = []
+
+    array = []
+
 end
 
 def array_with_two_elements
-    my_two_array = [1,2]
+
+    array = [0,1]
+
 end
 
 def first_element(array)
+
     array[0]
+
 end
 
 def third_element(array)
+
     array[2]
+
 end
 
 def last_element(array)
+
     array[-1]
+
 end
 
 def first_element_with_array_methods(array)
-    array.shift
+
+    firstElement = array.shift()
 
 end
 
 def last_element_with_array_methods(array)
-    array.pop
+
+    lastElement = array.pop()
 
 end
 
 def length_of_array(array)
-    array.length
+
+    arrayLength = array.length()
+
 end
