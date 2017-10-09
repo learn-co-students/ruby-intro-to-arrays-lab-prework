a9a30f7ae413e08853c4ea2d79fa45fa931e0c08
johneckert <johnteckert@gmail.com>
2017-10-09 12:41:20 -0400

    done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+# Instantiating
+
+def instantiate_new_array
+  new_array = []
+end
+
+def array_with_two_elements
+  new_two_array = [1,2]
+end
+
+# Indexing
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+# using ruby array methods to return values from an array
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+# using ruby array methods to get information about an array
+
+def length_of_array(array)
+  array.length
+end
