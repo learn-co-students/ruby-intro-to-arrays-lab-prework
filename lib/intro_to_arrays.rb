83259b88b2e834ba894cadfe6f98a69a71f59a4a
eal6gf <elum1234@gmail.com>
2017-10-27 23:52:41 -0400

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,19 +10,19 @@ def first_element(array)
   array[0]
 end
 
-def third_element
+def third_element(array)
   array[2]
 end
 
-def last_element
+def last_element(array)
   array[-1]
 end
 
-def first_element_with_array_methods
+def first_element_with_array_methods(array)
   array.first()
 end
 
-def last_element_with_array_methods
+def last_element_with_array_methods(array)
   array.last()
 end
 
