2f8df97828e7f38c734acd06934ca6d2397ebd7f
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-16 03:37:18 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  [0,1]
+end
+
+def first_element(ar)
+  ar[0]
+end
+
+def third_element(ar)
+  ar[2]
+end
+
+def last_element(ar)
+  ar[-1]
+end
+
+def first_element_with_array_methods(ar)
+  ar.first
+end
+
+def last_element_with_array_methods(ar)
+  ar.last
+end
+
+def length_of_array(ar)
+  ar.length
+end
