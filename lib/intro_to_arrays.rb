0ee9650c70c2fd1232585169787252d484e9ee94
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 04:48:30 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+new_arr = []
+end
+
+def array_with_two_elements
+  new_arr = ["jam","jelly"]
+  return new_arr
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array[0]
+end
+
+def last_element_with_array_methods(array)
+  return array[-1]
+end
+
+
+def length_of_array(array)
+  return array.length
+end
