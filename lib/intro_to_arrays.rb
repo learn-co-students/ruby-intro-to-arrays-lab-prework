7a85a7f43c00ddb4c83c38259b87a234bdd1c8ba
Eric Razor <eric.razorjr@gmail.com>
2018-04-03 04:46:27 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  newArray = []
+  return newArray
+end
+
+def array_with_two_elements
+  newArray = ["",""]
+  return newArray
+end
+
+def first_element(array)
+  first_element=array[0]
+  return first_element
+end
+
+def third_element(array)
+  third_element=array[2]
+  return third_element
+end
+
+def last_element(array)
+  last_element=array[-1]
+  return last_element
+end
+
+def first_element_with_array_methods(array)
+  return array[0]
+end
+
+def last_element_with_array_methods(array)
+  return array[-1]
+end
+
+def length_of_array(array)
+  length = array.length
+  return length
+end
