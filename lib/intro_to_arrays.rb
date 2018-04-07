53b3df4283f06b5d2feb7e679db1517ed5661f2b
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-07 03:47:28 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  []
+  return []
+end
+
+def array_with_two_elements
+["anum", "dani"]
+  return ["anum", "dani"]
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[array.length - 1]
+end
+
+def first_element_with_array_methods(array)
+  array.shift()
+end
+
+def last_element_with_array_methods(array)
+  array.pop()
+end
+
+def length_of_array(array)
+  array.count
+end
