a1baa6b635ba3ff306a2fdbdbdbf2cd286f16bd6
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-22 17:20:54 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,15 @@
+def my_new_array = []
+  return my_new_array
+end 
+
+def my_new_array = [1,2]
+  return my_new_array
+end 
+
+def taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    my_first_element = taylor_swift[0]
+    my_third_element = taylor_swift[2]
+    my_last_element = taylor_swift[-1]
+    
+end
+
