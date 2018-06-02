73c92afd24b16c308e22bf73c875ffeec00ce498
Laura Adam <laurawynosky@gmail.com>
2018-06-02 22:38:22 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+#working with arrays
+
+#instantiating=represent as or by an instance
+def instantiate_new_array
+    @my_new_array = instantiate_new_array
+end
+    
+def array_with_two_elements
+   @my_new_array = [num1, num2]   
+end 
+
+#indexing
+    def first_element
+      @my_new_array.first 
+    end
+      
+    def third_element
+      @my_new_array [3]
+    end
+  
+    def last_element
+      @my_new_array.last 
+    end
+      
+# using ruby array methods to return values froman array
+    def first_element_with_array_methods
+      @my_new_array.index (num1)
+    end
+    
+    def last_element_with_array_methods
+      @my_new_array. index (num2)
+    end  
+#using ruby array methods to get information about an array
+  def length_of_array
+      @my_new_array.size
+  end
