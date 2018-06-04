fc3ae07caff8280b8568d21dbe3ff98159532655
Jason Pitts <juniorjr083@gmail.com>
2018-06-04 22:27:05 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,60 @@
+def instantiate_new_array 
+  na = []
+  return na
+  
+  
+end 
+
+
+def array_with_two_elements 
+  na = [1,2]
+  
+  
+end 
+
+
+
+
+
+def first_element(arr)
+  return arr[0]
+  
+  
+end 
+
+
+
+
+def third_element(arr) 
+  return arr[2]
+  
+end 
+
+
+def last_element(arr) 
+  return arr[-1]
+  
+end
+
+
+
+def first_element_with_array_methods(arr) 
+  
+  return arr.first
+  
+end 
+
+
+def last_element_with_array_methods(arr) 
+  
+  return arr.last
+  
+end 
+
+
+def length_of_array(arr)
+  return arr.length
+  
+  
+end 
+
