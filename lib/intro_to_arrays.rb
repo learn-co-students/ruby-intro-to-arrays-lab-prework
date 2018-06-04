79e242be62a7a68dbaa83e223683b06cc0c44bd1
github username <github email address>
2018-06-04 16:37:12 -0400

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -32,31 +32,3 @@ end
 def length_of_array (array)
    array.size
 end
-
-
-
-#indexing
-#     def first_element()
-#       @my_new_array.first
-#     end
-#
-#     def third_element
-#       @my_new_array [3]
-#     end
-#
-#     def last_element
-#       @my_new_array.last
-#     end
-#
-# # using ruby array methods to return values froman array
-#     def first_element_with_array_methods
-#       @my_new_array.index (num1)
-#     end
-#
-#     def last_element_with_array_methods
-#       @my_new_array. index (num2)
-#     end
-# #using ruby array methods to get information about an array
-#   def length_of_array
-#       @my_new_array.size
-#   end
