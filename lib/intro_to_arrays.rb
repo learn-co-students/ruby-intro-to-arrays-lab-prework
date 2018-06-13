75e5054b1599ca5e5d3f3b111b48ab5a08d4000a
Laura Adam <laurawynosky@gmail.com>
2018-06-13 20:43:32 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,6 +2,7 @@
 
 #instantiating=represent as or by an instance
 def instantiate_new_array
+<<<<<<< HEAD
     Array.new
 end
 
@@ -32,3 +33,54 @@ end
 def length_of_array (array)
    array.size
 end
+=======
+   @my_two_array=instantiate_new_arrary
+end
+#returns an array that is empty
+   size=@my_new_array
+end
+
+#create an array with two elements    
+def array_with_two_elements
+   @my_two_array=array_with_two_elements 
+end 
+
+#create an array_with_two_elements_and_returns_it
+def array_with_two_elements
+   @my_two_array=array_with_two_elements_and_returns_it
+end 
+
+#returns_an_array_with_two_objects
+def array_with_two_objects
+   @my_two_array=array_with_two_objects
+end 
+
+#indexing
+
+  @taylor_swift=["Welcome to New York", "Blank Space", "Style", "Out of The Woods]
+  end  
+    
+    def first_element
+      @taylor.swift.first 
+    end
+      
+    def third_element
+      @taylor_swift [3]
+    end
+  
+    def last_element
+      @taylor_swift.last 
+    end
+  
+#using ruby array methods to return values froman array
+    def first_element_with_array_methods
+      @taylor_swift.index (0)
+    end
+    
+    def last_element_with_array_methods
+      @taylor_swift. index (4)
+    end  
+#using ruby array methods to get information about an array
+
+
+>>>>>>> eae9477a71a6a9c323a04318cccf7b1ea5cfbf99
