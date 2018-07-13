957d4e414dde04ebdc60745eb41ab15dbf1711d1
Sin Nam <sinnam90@gmail.com>
2018-07-13 03:21:28 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,31 +1,38 @@
+#instantiate_new_array
 def instantiate_new_array
   Array.new
 end
-
+#array_with_two_elements
 def array_with_two_elements
   array = [0,1]
 end
 
+#first_element
 def first_element(array)
   array[0]
 end
 
+#third_element
 def third_element(array)
   array[2]
 end
 
+#last_element
 def last_element(array)
   array[-1]
 end
 
+#first_element_with_array_methods
 def first_element_with_array_methods(array)
   array.first
 end
 
+#last_element_with_array_methods
 def last_element_with_array_methods(array)
   array.last
 end
 
+#length_of_array
 def length_of_array(array)
   array.size
 end
