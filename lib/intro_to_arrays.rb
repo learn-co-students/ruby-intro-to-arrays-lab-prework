d85e42020a627ac20ae52d5b711693e59a8d99c8
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-19 22:30:10 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,9 +1,9 @@
-def instantiate_new_array 
-  new_array = []
+def instantiate_new_array
+  []
 end
 
-def array_with_two_elements 
-  cats = ["Nala", "Simba"]
+def array_with_two_elements
+  [1, 2]
 end
 
 def first_element(array)
@@ -19,7 +19,7 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.first
+  array.first 
 end
 
 def last_element_with_array_methods(array)
@@ -29,3 +29,55 @@ end
 def length_of_array(array)
   array.length 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def instantiate_new_array 
+#   new_array = []
+# end
+
+# def array_with_two_elements 
+#   cats = ["Nala", "Simba"]
+# end
+
+# def first_element(array)
+#   array[0]
+# end
+
+# def third_element(array)
+#   array[2]
+# end
+
+# def last_element(array)
+#   array[-1]
+# end
+
+# def first_element_with_array_methods(array)
+#   array.first
+# end
+
+# def last_element_with_array_methods(array)
+#   array.last 
+# end
+
+# def length_of_array(array)
+#   array.length 
+# end
