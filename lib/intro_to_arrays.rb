6e6fe9bacc01464419adad34d15536c424cf0c04
Charlie Russo <cparusso@gmail.com>
2018-07-28 18:24:53 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  array = []
+  array
+end
+
+def array_with_two_elements
+  anArr = ["one", 2]
+  anArr
+end
+
+def first_element(anArr)
+  anArr[0]
+end
+
+def second_element(anArr)
+  anArr[1]
+end
+
+def third_element(anArr)
+  anArr[2]
+end
+
+def last_element(anArr)
+  anArr[-1]
+end
+
+def first_element_with_array_methods(anArr)
+  anArr.first()
+end
+
+def last_element_with_array_methods(anArr)
+  anArr.last()
+end
+
+def length_of_array(anArr)
+  anArr.length()
+end
