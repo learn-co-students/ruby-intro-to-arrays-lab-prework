58ad95817f76bdc59832aeb42e996dbdbf50dd57
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-10 19:56:38 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+    @my_new_array = []
+  @my_new_array
+end
+
+def array_with_two_elements
+  @my_new_array = [1, 2]
+  @my_new_array
+end
+
+def first_element(array)
+  my_first_element = @taylor_swift[0]
+end
+
+def third_element(array)
+  my_third_element = @taylor_swift[2]
+end
+
+def last_element(array)
+  my_last_element = @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(array)
+  first_country = @south_east_asia.first
+end
+
+def last_element_with_array_methods(array)
+  last_country = @south_east_asia.last
+end
+
+def length_of_array(array)
+  length = @programming_languages.length
+end
