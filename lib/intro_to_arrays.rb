29cac5df9600c9e96046b8e4218cdf2c6b4f69e2
FutureScript <fernandobaxter1@gmail.com>
2018-11-13 13:12:15 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,27 @@ end
 def array_with_two_elements
   [1,2]
 end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.shift
+end
+
+def last_element_with_array_methods(array)
+  array.pop
+end
+
+def length_of_array(array)
+  array.length
+end 
