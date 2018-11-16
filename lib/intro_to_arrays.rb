1e60587443749db129f0b0fe9ef3b870f3ec6481
Megan Fago <meganlaynefago@gmail.com>
2018-11-16 22:14:14 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+ instantiate_new_array = []
+end
+
+def array_with_two_elements
+  array_with_two_elements = [1, 2]
+end
+
+def first_element(my_first_element)
+  my_first_element[0]
+end
+
+def third_element(my_third_element)
+  my_third_element[2]
+end
+
+def last_element(my_last_element)
+  my_last_element.last
+end
+
+def first_element_with_array_methods(first_country)
+  first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+  last_country.last
+end
+
+def length_of_array(programming_languages)
+  programming_languages.length
+end
+  
