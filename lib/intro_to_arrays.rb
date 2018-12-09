807adea980648db99368def69a8b3bd6cd5c1475
emily seieroe <e.e.seieroe@gmail.com>
2018-12-09 00:13:39 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,4 +7,34 @@ def array_with_two_elements
   ["x", "b"]
 end
 
-def first_element
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[array.length - 1]
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
+
+
+
+
+
+
+
+  
