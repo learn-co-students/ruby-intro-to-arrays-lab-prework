c20fbbaea62d06eadedf4419975d21759b4397cd
Andrea Williams <anw0514@gmail.com>
2018-12-30 13:33:28 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  testarray = []
+end
+  
+def array_with_two_elements
+  testarray = ["object1", "object2"]
+end
+
+def first_element(a)
+  a.first
+end
+
+def third_element(a)
+  a[2]
+end
+
+def last_element(a)
+  a.last
+end
+  
+def first_element_with_array_methods(a)
+  a.first
+end
+  
+def last_element_with_array_methods(a)
+  a.last
+end
+
+def length_of_array(a)
+  a.length
+end
+
+  
