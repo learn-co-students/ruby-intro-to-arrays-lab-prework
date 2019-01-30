af985000718cc55a4d4f9df373194199a16cd479
Burak Kocak <burakkocak884@ghotmail.com>
2019-01-30 16:55:12 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,26 @@
+
+def instantiate_new_array
+Array.new
+end
+def array_with_two_elements
+	array = [0, 2]
+end
+def first_element(array)
+array[0]
+end
+def third_element(array)
+array[2]
+end
+def last_element(array)
+
+	array[-1]
+end
+def first_element_with_array_methods(south_east_asia)
+south_east_asia[0]
+	end
+	def last_element_with_array_methods(south_east_asia)
+		south_east_asia[3]
+	end
+	def length_of_array(programming_languages)
+		programming_languages.length
+	end
