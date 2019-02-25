03f4fc98259e9073ec0f1db050e0de1284a4fb1b
Bob Habig <rfhab2@gmail.com>
2019-02-25 04:04:38 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1,2]
+end
+
+def first_element(array_arg)
+  array_arg[0]
+end
+
+def third_element(array_arg)
+  array_arg[2]
+end
+
+def last_element(array_arg)
+  array_arg[-1]
+end
+
+def first_element_with_array_methods(array_arg)
+  array_arg.first
+end
+
+def last_element_with_array_methods(array_arg)
+  array_arg.last
+end
+
+def length_of_array(array_arg)
+  array_arg.length
+end
