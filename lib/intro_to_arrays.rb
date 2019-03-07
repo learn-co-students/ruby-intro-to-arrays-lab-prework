01dc81708cbd8b3dd397db2ab1ec9559288de40d
David zhang <d.ydzdavid@yahoo.com>
2019-03-07 12:34:05 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+[1, 2]
+end
+
+def first_element(array)
+  "Welcome to New York"
+end
+
+def third_element(array)
+"Style"
+end
+
+def last_element(array)
+  "Out of The Woods"
+end
+
+def first_element_with_array_methods(array)
+  "Thailand"
+end
+
+def last_element_with_array_methods(array)
+  "Myanmar"
+end
+
+def length_of_array(array)
+8
+  end
