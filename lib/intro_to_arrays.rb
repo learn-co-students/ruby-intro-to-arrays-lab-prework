4fdb139799a22f2fd042e7c381fea670471a3f93
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-25 18:38:53 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  newArr = []
+end  
+def array_with_two_elements
+  newArr = ['a', 'b']
+end  
+def first_element(arr)
+  arr[0]
+end
+def third_element(arr)
+  arr[2]
+end  
+def last_element(arr)
+  arr[-1]
+end  
+def first_element_with_array_methods(arr)
+  firstElem = arr.shift
+end
+def last_element_with_array_methods(arr)
+  lastElem = arr.pop
+end  
+def length_of_array(arr)
+  arr.length
+end  
