5504d7aa896df279112ace448548de5000149646
Dylan Harris <dylanharris23@gmail.com>
2019-05-29 04:35:24 +0000

    Done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,30 @@
+def instantiate_new_array
+  array = []
+end
+def array_with_two_elements
+  array = ["Hello", "Goodbye"]
+end
+def first_element(taylor_swift)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    taylor_swift.first
+end
+def third_element(taylor_swift)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    taylor_swift[2]
+  end
+def last_element(taylor_swift)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    taylor_swift.last
+  end
+def first_element_with_array_methods(south_east_asia)
+    south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    south_east_asia.first
+  end
+def last_element_with_array_methods(south_east_asia)
+    south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    south_east_asia.last
+  end
+def length_of_array(numbera)
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+    numbers[7]
+  end
