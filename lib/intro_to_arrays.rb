50245712d3bc3963a304c40f8e0b720334bf028b
David Fellows <dfellows12@gmail.com>
2019-05-29 16:38:56 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  myArr = []
+  myArr
+end
+
+def array_with_two_elements
+  myArr = ["Techno", "House"]
+  myArr
+end
+
+def first_element(myArr)
+  return myArr[0]
+end
+
+def third_element(myArr)
+  return myArr[2]
+end
+
+def last_element(myArr)
+  return myArr[-1]
+end
+
+def first_element_with_array_methods(myArr)
+  return myArr.first
+end
+
+def last_element_with_array_methods(myArr)
+  return myArr.last
+end
+
+def length_of_array(myArr)
+  return myArr.length
+end
